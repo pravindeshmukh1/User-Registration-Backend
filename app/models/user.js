5bee07890d8d76e95f6e6ae3b1b535c8eaa74e23
pravindeshmukh1 <pravin1desh@gmail.com>
2020-10-08 15:58:46 +0530

    add: create user schema

---
+++ app/models/user.js
@@ -0,0 +1,48 @@
+/*******************************************************************************************
+ * @Purpose   : User Database Schema create and Validation
+ * @file      : userModel.js
+ * @overview  : create schema for user registration and schema level validation
+ * @author    : PRAVIN DESHMUKH
+ * @since     : 08/10/2020
+ *******************************************************************************************/
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "Firstname is required"],
+      trim: true,
+      length: { min: 3, max: 10 },
+    },
+    lastName: {
+      type: String,
+      required: [true, "Lastname is required"],
+      length: { min: 3, max: 10 },
+      trim: true,
+    },
+    emailId: {
+      type: String,
+      required: [true, "Email Id is required"],
+      index: {
+        unique: true,
+      },
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [8, "Password must be atleast 8 character"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userModel = mongoose.model("User", userSchema);
+
+class UserModel {}
+
+module.exports = new UserModel();
